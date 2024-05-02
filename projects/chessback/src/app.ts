52ab67998f779b02d9ee2a06a892aed25445f8c5
Yohann Rabatel <yohann.rabatel@gmail.com>
2024-05-02 22:26:08 +0200

    up libs

--- projects/chessback/src/app.ts
+++ projects/chessback/src/app.ts
@@ -1,40 +1,39 @@
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
-import { applyWSSHandler } from '@trpc/server/adapters/ws';
-import cors from 'cors';
-import ws from 'ws';
-import { createContext } from './services/context';
-import appRouter from './router';
+import { createHTTPServer } from '@trpc/server/adapters/standalone'
+import { applyWSSHandler } from '@trpc/server/adapters/ws'
+import cors from 'cors'
+import ws from 'ws'
+import { createContext } from './services/context'
+import appRouter from './router'
 
-const port = +(process.env.PORT || 4001);
+const port = +(process.env.PORT || 4001)
 
 // Export type router type signature,
 // NOT the router itself.
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
 
 const httpServer = createHTTPServer({
-    middleware: cors(),
-    router: appRouter,
-    createContext
-});
+	middleware: cors(),
+	router: appRouter,
+	createContext
+})
 
 const wss = new ws.Server({
-    server: httpServer.server
-});
+	server: httpServer
+})
 
-const handler = applyWSSHandler({ wss, router: appRouter, createContext });
-wss.on('connection', (ws) => {
-    console.log(`new connection (${wss.clients.size})`);
-    ws.once('close', () => {
-        console.log(`closed connection (${wss.clients.size})`);
-    });
-});
+const handler = applyWSSHandler({ wss, router: appRouter, createContext })
+wss.on('connection', ws => {
+	console.log(`new connection (${wss.clients.size})`)
+	ws.once('close', () => {
+		console.log(`closed connection (${wss.clients.size})`)
+	})
+})
 
 process.on('SIGTERM', () => {
-    console.log('SIGTERM');
-    handler.broadcastReconnectNotification();
-    wss.close();
-});
+	console.log('SIGTERM')
+	handler.broadcastReconnectNotification()
+	wss.close()
+})
 
-
-httpServer.listen(port);
-console.log(`✅ Http & Ws Server listening on http://localhost:${port}`);
+httpServer.listen(port)
+console.log(`✅ Http & Ws Server listening on http://localhost:${port}`)
